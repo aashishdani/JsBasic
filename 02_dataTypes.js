6919a149cc19b2e907f3c1dadce593915a808e25
aashishdani <109866017+aashishdani@users.noreply.github.com>
2024-07-23 19:28:35 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";          // treat all js code as newer version
+
+// alert( 3 + 3 )      // we are using nodejs, not browser
+
+console.log("Amit");   // code readablity should be high.
+
+let name = "Akshay"
+let age = 20
+let isLogged = false
+
+// Number  =>   2 to power 53
+// bigInt
+// string
+// boolean =>   true or false
+// null =>      standalone value (It is an object)
+// undefined => 
+// Symbol    =>  unique
+// Object    =>  
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // Object
+console.log(typeof undefined);
